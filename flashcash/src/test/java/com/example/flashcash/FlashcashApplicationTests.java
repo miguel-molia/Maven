7c9398c1f53f3ccb26fd7e6800291f4d0a0db02b
miguel-molia <miguel.molia@laplateforme.io>
2024-03-18 13:54:24 +0100

    Initial commit

---
+++ flashcash/src/test/java/com/example/flashcash/FlashcashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flashcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
