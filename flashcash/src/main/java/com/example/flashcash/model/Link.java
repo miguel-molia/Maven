f77560475a1e3e89126af34aaa2f352d1a3fb586
miguel-molia <miguel.molia@laplateforme.io>
2024-03-22 11:36:14 +0100

    update

--- flashcash/src/main/java/com/example/flashcash/model/Link.java
+++ flashcash/src/main/java/com/example/flashcash/model/Link.java
@@ -1,4 +1,18 @@
 package com.example.flashcash.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
 public class Link {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+Integer id;
+@ManyToOne
+User user1;
+@ManyToOne
+User user2;
 }
--- flashcash/src/main/java/com/example/flashcash/model/Transfer.java
+++ flashcash/src/main/java/com/example/flashcash/model/Transfer.java
@@ -1,4 +1,23 @@
 package com.example.flashcash.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Transfer {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Integer id;
+private LocalDateTime date;
+@ManyToOne
+private User from;
+@ManyToOne
+private User to;
+private Double amountBeforeFee;
+private Double amountAfterFee;
 }
