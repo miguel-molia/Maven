2d6f47d8f5c488f87ffb6989441b7529f13448bb
miguel-molia <miguel.molia@laplateforme.io>
2024-03-25 00:39:48 +0100

    update

--- flashcash/src/main/java/com/example/flashcash/config/MvcConfig.java
+++ flashcash/src/main/java/com/example/flashcash/config/MvcConfig.java
@@ -1,4 +1,14 @@
 package com.example.flashcash.config;
 
-public class MvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/signin").setViewName("signin");
+    }
 }
--- flashcash/src/main/java/com/example/flashcash/config/SecurityConfiguration.java
+++ flashcash/src/main/java/com/example/flashcash/config/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.example.flashcash.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -11,5 +13,27 @@ public class SecurityConfiguration {
     public class PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(requests) -> requests
+
+                    .requestMatchers("/bootstrap.min.css","/index.css","/images/**","/signin",")
+                            .anyRequest().authenticated()
+                    )
+
+
+                .formLogin((form) -> form
+                        .loginPage("/signin")
+                        .permitAll().usernameParameter("email").defaultSuccessUrl(defaultSuccessUrl "/", alwaysUse: true)
+
+        )
+        .logout((logout) -> logout.permitAll());
+
+        n http.build();
+
     }
 }
