c69092c95d96bf70a59c8e2d165ee124c06d9e62
miguel-molia <miguel.molia@laplateforme.io>
2024-04-05 15:24:36 +0200

    update

---
+++ HealthCare/eurekaserver/src/test/java/com/miguelmolia/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelmolia.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
